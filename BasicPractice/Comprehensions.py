f9fa85e6756ffc037d781a1714a382246e2d4678
waridhasan195 <waridhasan195@gmail.com>
2022-11-13 09:59:06 +0600

    More Program

--- BasicPractice/Comprehensions.py
+++ BasicPractice/Comprehensions.py
@@ -9,22 +9,41 @@
 # x = [i for i in range(100) if i%2 == 0]
 # print(x)
 
+# x = [i for i in range(int(input())) if i%2==0 and i!=0]
+# print(x)
+
 
 # x = [i+j for i in range(10) for j in range(10)]
 # print(x)
 
+# x = [i+j for i in range(int(input("I: "))) for j in range(int(input("j: ")))]
+# print(x)
+
+
 # x = [[_ for _ in range(5)] for _ in range(5)]
 # print(x)
+
+# x = [[_ for _ in range(6)] for _ in range(6)]
+# print(x)
+
+
 
 # x = (i for i in "Hello")
 # print(tuple(x))
 
+# x = [i for i in "Hello"]
+# xx = x[-1] #Last Item
+# print(xx)
+# print(x)
 
-sentence = "Hello my name is warid"
-sett = set(sentence)
-print(sett)
-x = {char: sentence.count(char) for char in set(sentence)}
-print(x)
+
+
+
+# sentence = "How to get lorem ipsum to instantiate on multiple lines instead ..."
+# # sett = set(sentence)
+# # print(sett)
+# # x = {char: sentence.count(char) for char in set(sentence)}
+# # print(x)
 
 
 
---
+++ BasicPractice/a.py
@@ -0,0 +1,14 @@
+
+
+# import calendar
+
+# day = int(input())
+# month = int(input())
+# print(calendar.month(day, month))
+
+
+
+
+
+x = (list((int(input()) for i in range(int(input("inputNumber: "))) )))
+print(x)
